e5f56878b808ab2c27786de58c313b00bf258526
swnam <swnam@dkitec.com>
2020-08-03 09:57:58 +0900

    test

--- ProtocolTester/TestCase/PlayerTestCase.swift
+++ ProtocolTester/TestCase/PlayerTestCase.swift
@@ -18,10 +18,6 @@ extension TestMainViewController {
                        movieId: package["movieAssetId"].stringValue,
                        payYn: true,
                        resumeYn: false)
-            
-            
-            
-            
         }
     }
 }
