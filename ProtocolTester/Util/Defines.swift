5b56f16380656439a3106eb0b7380bc7057ff918
swnam <swnam@dkitec.com>
2020-08-06 11:36:34 +0900

    commit

--- ProtocolTester/Util/Defines.swift
+++ ProtocolTester/Util/Defines.swift
@@ -11,13 +11,12 @@ import Foundation
 class Defines {
 //    static let baseUrl = "http://222.122.207.37/service" // TB
     static let baseUrl = "http://59.15.95.42:4090/service" // INNERWAVE
+    static let recommendUrl = "http://222.122.207.46:8080/rec"    // TB
+    static let searchUrl = "http://222.122.207.44:8080/ser"       // TB
     
-//    static let recommendUrl = "http://222.122.207.46:8080/rec"    // TB
-//    static let searchUrl = "http://222.122.207.44:8080/ser"       // TB
-    
-    static let recommendUrl = "http://iptv.recommendrec.3bbtv.com:8080/rec" // Staging
-    static let searchUrl = "http://iptv.totalser.3bbtv.com:8080/ser"        // Staging
-    
+//    static let baseUrl = "http://59.15.95.42:4090/service"                  // Staging
+//    static let recommendUrl = "http://iptv.recommendrec.3bbtv.com:8080/rec" // Staging
+//    static let searchUrl = "http://iptv.totalser.3bbtv.com:8080/ser"        // Staging
     
     static let laUrl = " https://iptv.license.3bbtv.com/LIC_REQ_CC?ContentID=265&ServiceType=1&LoginToken=DPw0DEJsdzeECCJav44D1o2zzpCeo5ps3PZLUFYqeEdo6uoVCVZ95TOSnKnA5&ProfileID=TV80008035000&OTU=3231&DownloadYN=N&deviceType=OTA"
 }
